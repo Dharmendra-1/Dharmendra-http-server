37a6698e7ce986fe47b53b6f11e2f0f566a2ffae
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-19 19:39:44 +0530

    remove custom promise and used fs promise for reading file..

--- server/router.js
+++ server/router.js
@@ -1,21 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
-const fs = require('fs');
+const fsPromise = require('fs').promises;
 const uniqueId = require('uuidv4');
 const { response } = require('express');
 const HTTP = require('http');
-
-const readFile = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, 'utf-8', (error, data) => {
-      if (error) {
-        reject('Filename is not correct..');
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
 
 router.get('/', (request, response, next) => {
   response.status(200).send(`<h1>Welcome</h1>`);
@@ -29,14 +17,17 @@ router.get('/html', (request, response, next) => {
 });
 
 router.get('/data', (request, response, next) => {
-  readFile(path.join(__dirname, '../data/data.json'))
+  fsPromise
+    .readFile(path.join(__dirname, '../data/data.json'))
     .then((jsonData) => {
       response.setHeader('Content-Type', 'application/json');
       response.send(jsonData);
       next();
     })
     .catch((error) => {
-      response.status(400).send(`<h1>THis is Server Error: ${error}</h1>`);
+      response
+        .status(404)
+        .send(`<h1>File Path Does not exists And Error is ${error}</h1>`);
       next();
     });
 });
