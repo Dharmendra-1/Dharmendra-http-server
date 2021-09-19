59fa9e9612670fbfd9efe6b27fc84600c54799b4
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-19 19:10:29 +0530

    import express, router and used first middleware for router and last one is for 404 Error handler and listining on Port.

--- server/index.js
+++ server/index.js
@@ -1,105 +1,17 @@
-const http = require('http');
-const fs = require('fs');
-const uniqueId = require('uuidv4');
-const path = require('path');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
+const router = require('./router');
 
-const readFile = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, 'utf-8', (error, data) => {
-      if (error) {
-        reject('Filename is not correct..');
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+app.use(router);
 
-const server = http.createServer((request, response) => {
-  const url = request.url.split('/');
-  const URL = url[1];
-
-  switch (URL) {
-    case 'html':
-      readFile(path.join(__dirname, '../public/index.html'))
-        .then((content) => {
-          response.writeHead(200, { 'content-type': 'text/html' });
-          response.write(content);
-          response.end();
-        })
-        .catch((error) => {
-          response.writeHead(404, { 'content-type': 'text/html' });
-          response.write('File Not Found and Error :' + error);
-          response.end();
-        });
-      break;
-    case 'data':
-      readFile(path.join(__dirname, '../data/data.json'))
-        .then((content) => {
-          response.writeHead(200, { 'content-type': 'application/json' });
-          response.write(content);
-          response.end();
-        })
-        .catch((error) => {
-          response.writeHead(404, { 'content-type': 'application/json' });
-          response.write('File Not Found and Error :' + error);
-          response.end();
-        });
-      break;
-    case 'uuid':
-      const idObj = { uuid: uniqueId.uuid() };
-      response.writeHead(200, { 'content-type': 'application/json' });
-      response.write(JSON.stringify(idObj));
-      response.end();
-      break;
-    case 'status':
-      const statusCodeObject = http.STATUS_CODES;
-      const requestedSatusCode = path.basename(request.url);
-
-      if (
-        requestedSatusCode in statusCodeObject &&
-        request.url === `/status/${requestedSatusCode}`
-      ) {
-        response.writeHead(parseInt(requestedSatusCode), {
-          'Content-Type': 'application/json',
-        });
-        response.write(
-          JSON.stringify({
-            [requestedSatusCode]: statusCodeObject[requestedSatusCode],
-          })
-        );
-        response.end();
-      } else {
-        response.writeHead(404, { 'content-type': 'text/html' });
-        response.write(`<h1>Invalid ${requestedSatusCode} code</h1>`);
-        response.end();
-      }
-
-      break;
-    case 'delay':
-      let timeDelay = Math.abs(path.basename(request.url) * 1000);
-      setTimeout(() => {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(`<h1>Response after ${timeDelay / 1000} seconds`);
-        response.end();
-      }, timeDelay);
-      break;
-    default:
-      if (!path.basename(request.url)) {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(
-          `<h1>This is main server try searching with child urls</h1>`
-        );
-        response.end();
-      } else {
-        response.writeHead(404, { 'content-type': 'text/html' });
-        response.write(`<h1> check the url<h1/>`);
-        response.end();
-      }
-  }
+app.use((request, response, next) => {
+  response
+    .status(404)
+    .send('<h1>Unable to find the requested resource! 404 Not Founds</h1>');
+  next();
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
